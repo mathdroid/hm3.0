ce6faf84f67b118ca5222a7b6c6f3a907b4f6f5c
johanridho <johan.ridho@gmail.com>
2015-12-06 19:54:49 +0700

    added watcher & komplain kemandekan

--- client/app/app.js
+++ client/app/app.js
@@ -49,24 +49,43 @@ angular.module('sikk', ['ngRoute','firebase'])
 .controller('CaseDetailController', ["$scope", "$routeParams", "$firebaseArray", "FirebaseUrl",  function($scope, $routeParams, $firebaseArray, FirebaseUrl) {
 	var ref = new Firebase(FirebaseUrl + "cases");
     var caseArray = $firebaseArray(ref);
+    $scope.currentProcess = 4;
 	caseArray.$loaded().then(function(){
     	$scope.case = caseArray.$getRecord($routeParams.id);
     })
     .catch(function(error) {
 	    console.log("Error:", error);
 	});
+    $scope.incrementWatcher = function(data) {        
+      data.watcher++;
+      caseArray.$save(data);      
+    }
+    $scope.incrementKemandekan = function(data) {        
+      data.komplain_kemandekan++;
+      caseArray.$save(data);      
+    }
 }])
 
 .controller('CaseListController', ["$scope", "$routeParams", "$firebaseArray", "FirebaseUrl", function($scope, $routeParams, $firebaseArray, FirebaseUrl) {
     var ref = new Firebase(FirebaseUrl + "cases");
     $scope.cases = $firebaseArray(ref);
+
+    $scope.incrementWatcher = function(data) {        
+      data.watcher++;
+      $scope.cases.$save(data);      
+    }
 }])
 
 .controller('CorruptorDetailController', ["$scope", "$routeParams", "$firebaseArray", "FirebaseUrl",  function($scope, $routeParams, $firebaseArray, FirebaseUrl) {
     var ref = new Firebase(FirebaseUrl + "corruptors");
-    var corruptorArray = $firebaseArray(ref);
+    var corruptorArray = $firebaseArray(ref);    
     corruptorArray.$loaded().then(function(){
         $scope.corruptor = corruptorArray.$getRecord($routeParams.id);
+        if($scope.corruptor.value >= 10000000000){
+            $scope.corruptorGrade = "Kakap";
+        }else{
+            $scope.corruptorGrade = "Teri";
+        }
     })
     .catch(function(error) {
         console.log("Error:", error);
